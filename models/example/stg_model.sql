42f6280ce1b183ada85af6fb4d81f4bf54545f6f
Adam Výborný <mail@adamvyborny.cz>
2023-03-30 14:48:28 +0200

    Test shared bucket

--- models/example/stg_model.sql
+++ models/example/stg_model.sql
@@ -16,3 +16,22 @@
     )
     
     select * from renamed
+    
+    with sourceShared as (
+        
+        select * from {{ source('in.c-demo-keboola-ex-google-drive-963568866', 'account') }}
+        
+    ),
+    
+    renamedShared as (
+        
+        select
+            "Id",
+            "Name",
+            "Region",
+            "Status"
+        from sourceShared
+    
+    )
+    
+    select * from renamedShared
