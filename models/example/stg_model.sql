49c25bdc3bec48889e12b1bbc6086b0db6bd7af9
Adam Výborný <mail@adamvyborny.cz>
2023-03-30 14:48:54 +0200

    Update stg_model.sql

--- models/example/stg_model.sql
+++ models/example/stg_model.sql
@@ -17,21 +17,3 @@
     
     select * from renamed
     
-    with sourceShared as (
-        
-        select * from {{ source('in.c-demo-keboola-ex-google-drive-963568866', 'account') }}
-        
-    ),
-    
-    renamedShared as (
-        
-        select
-            "Id",
-            "Name",
-            "Region",
-            "Status"
-        from sourceShared
-    
-    )
-    
-    select * from renamedShared
